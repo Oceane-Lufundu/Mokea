98bf28fdcdc21c06df2f94660e19e3b660115bc7
Oceane-Lufundu <lufundu.oceane@gmail.com>
2025-12-01 13:43:57 +0100

    scss inscription/connexion

---
+++ .angular/cache/20.3.10/mokea/vite/deps_ssr/@angular_ssr.js
@@ -0,0 +1,36 @@
+import { createRequire } from 'module';const require = createRequire(import.meta.url);
+import {
+  AngularAppEngine,
+  InlineCriticalCssProcessor,
+  PrerenderFallback,
+  RenderMode,
+  createRequestHandler,
+  destroyAngularServerApp,
+  extractRoutesAndCreateRouteTree,
+  getOrCreateAngularServerApp,
+  getRoutesFromAngularRouterConfig,
+  provideServerRendering,
+  setAngularAppEngineManifest,
+  setAngularAppManifest,
+  withAppShell,
+  withRoutes
+} from "./chunk-QUUMDXPD.js";
+import "./chunk-TXFH4YAZ.js";
+import "./chunk-G2SETQIJ.js";
+import "./chunk-6DU2HRTW.js";
+export {
+  AngularAppEngine,
+  PrerenderFallback,
+  RenderMode,
+  createRequestHandler,
+  provideServerRendering,
+  withAppShell,
+  withRoutes,
+  InlineCriticalCssProcessor as ɵInlineCriticalCssProcessor,
+  destroyAngularServerApp as ɵdestroyAngularServerApp,
+  extractRoutesAndCreateRouteTree as ɵextractRoutesAndCreateRouteTree,
+  getOrCreateAngularServerApp as ɵgetOrCreateAngularServerApp,
+  getRoutesFromAngularRouterConfig as ɵgetRoutesFromAngularRouterConfig,
+  setAngularAppEngineManifest as ɵsetAngularAppEngineManifest,
+  setAngularAppManifest as ɵsetAngularAppManifest
+};
