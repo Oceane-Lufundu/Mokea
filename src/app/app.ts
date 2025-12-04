ec0e55a1349678fb35b8a973b5fc8d595176e6e9
Oceane-Lufundu <lufundu.oceane@gmail.com>
2025-12-04 15:35:49 +0100

    ajout acceuil

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,14 @@
 import { Component, signal } from '@angular/core';
+import { bootstrapApplication } from '@angular/platform-browser';
 import { Navbar } from './navbar/navbar';
+import { Connexion } from './connexion/connexion';
+import { Inscription } from './inscription/inscription';
 import { Acceuil } from './acceuil/acceuil';
 import { Dashboard } from './dashboard/dashboard';
 
 @Component({
   selector: 'app-root',
-  imports: [Navbar,Acceuil,Dashboard],
+  imports: [ Inscription ] ,
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
